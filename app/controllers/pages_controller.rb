2e6540e96da661732057bd453865426bd91adc9e
andre <andrecabralpires@gmail.com>
2022-02-24 09:40:30 +0100

    search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    #can only be access if logged in
+    @user = current_user
+    @offers = @user.offers
+  end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,19 @@
 class Movie < ApplicationRecord
   belongs_to :director
+  include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
+
+  pg_search_scope :my_search,
+    against: [ :title, :synopsis ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+   pg_search_scope :global_search,
+    against: [ :title, :synopsis ],
+    associated_against: {
+      director: [ :first_name, :last_name ] ## add this to add association
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
